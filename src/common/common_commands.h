1fa9957b133474a5b29a7f5195eaaf06fd429598
priusf0x <priusfoxlittle@gmail.com>
2025-10-20 01:13:26 +0300

    Start binary work

--- src/common/common_commands.h
+++ src/common/common_commands.h
@@ -3,30 +3,15 @@
 
 enum commands_e
 {
-    COMMAND_HLT                 = 0,
-    COMMAND_PUSH                = 1,
-    COMMAND_OUT                 = 2,
-    COMMAND_ADD                 = 3,
-    COMMAND_SUB                 = 4,
-    COMMAND_MUL                 = 5,
-    COMMAND_DIV                 = 6,
-    COMMAND_PUSH_FROM_REG       = 7,
-    COMMAND_POP                 = 8,
-    COMMAND_SQRT                = 9,
-    COMMAND_IN                  = 10,
-    COMMAND_JMP                 = 11,
-    COMMAND_JA                  = 12,
-    COMMAND_JAE                 = 13,
-    COMMAND_JB                  = 14,
-    COMMAND_JBE                 = 15,
-    COMMAND_JE                  = 16,
-    COMMAND_JNE                 = 17,
-    COMMAND_CALL                = 18,
-    COMMAND_RET                 = 19,
-    COMMAND_PUSH_FROM_MEMORY    = 20,
-    COMMAND_POP_TO_MEMORY       = 21,
-    COMMAND_DRAW_B              = 22,
-    COMMAND_DRAW                = 23
+    COMMAND_HLT                 = 0, //0   |0000|000
+    COMMAND_PUSH                = 1, //0(1)|0001| 000(111)
+    COMMAND_OUT                 = 2, //0   |0010| 000
+    COMMAND_OPERATION           = 3, //0   |0011| 001(111)
+    COMMAND_POP                 = 4, //0   |0100| 000(111)
+    COMMAND_IN                  = 5, //0   |0101| 000(111)
+    COMMAND_JMP                 = 6, //0   |0110| 000(111)
+    COMMAND_CALL                = 7, //0   |0111| 000(001)
+    COMMAND_DRAW                = 8, //0   |1000| 000(
 };
 
 const char* const PROCESSORS_REG[] =
