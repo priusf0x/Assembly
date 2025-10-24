a16d3edefb92f1d59e1f831c685b99076b394824
priusf0x <priusfoxlittle@gmail.com>
2025-10-24 11:38:02 +0300

    Clean Up

--- src/disasm/disassembler.cpp
+++ src/disasm/disassembler.cpp
@@ -3,20 +3,24 @@
 #include <stdint.h>
 #include <unistd.h>
 
+#include "Assert.h"
 #include "disassembler_commands.h"
 #include "common_commands.h"
 #include "color.h"
 #include "simple_parser.h"
 #include "tools.h"
 
-enum disassembler_main_return_e
+enum disassembler_return_e
 {
-    DISASSEMBLER_MAIN_RETURN_SUCCESS,
-    DISASSEMBLER_MAIN_RETURN_READ_FLAG_ERROR,
-    DISASSEMBLER_MAIN_RETURN_ASSEMBLED_FILE_OPEN_ERROR,
-    DISASSEMBLER_MAIN_RETURN_DISASSEMBLED_FILE_OPEN_ERROR,
-    DISASSEMBLER_MAIN_RETURN_VERSION_MISMATCH
+    DISASSEMBLER_RETURN_SUCCESS,
+    DISASSEMBLER_RETURN_READ_FLAG_ERROR,
+    DISASSEMBLER_RETURN_ASSEMBLED_FILE_OPEN_ERROR,
+    DISASSEMBLER_RETURN_DISASSEMBLED_FILE_OPEN_ERROR,
+    DISASSEMBLER_RETURN_VERSION_MISMATCH
 };
+
+static disassembler_return_e ReadAssemblyFile(size_t* instructions_count, uint8_t** instructions);
+static disassembler_return_e WriteDisassembledFile(size_t instructions_count, uint8_t* instructions);
 
 const char* ASSEMBLED_FILE_NAME = "compiled.obj";
 const char* DISASSEMBLED_FILE_NAME = "disassembled.asm";
@@ -27,50 +31,79 @@ int
 main(int                argc,
      const char* const* argv)
 {
+    int output = 0;
     size_t instructions_count = 0;
-    uint64_t compiler_version = 0;
+    uint8_t* instructions = NULL;
 
-    if (ReadFlags(argc, argv, &ASSEMBLED_FILE_NAME, &DISASSEMBLED_FILE_NAME) != (int) DISASSEMBLER_MAIN_RETURN_SUCCESS)
+    if ((output = ReadFlags(argc, argv, &ASSEMBLED_FILE_NAME, &DISASSEMBLED_FILE_NAME)) != 0)
     {
         printf(RED "FLAG READ ERROR.\n" STANDARD);
 
-        return DISASSEMBLER_MAIN_RETURN_READ_FLAG_ERROR;
+        return output;
     }
+
+    if ((output = ReadAssemblyFile(&instructions_count, &instructions)) != 0)
+    {
+        printf(RED "READ ASSEMBLY FILE ERROR.\n" STANDARD);
+
+        return output;
+    }
+
+    if ((output = WriteDisassembledFile(instructions_count, instructions)) != 0)
+    {
+
+        return output;
+    }
+
+    free(instructions);
+
+    return DISASSEMBLER_RETURN_SUCCESS;
+}
+
+static disassembler_return_e
+ReadAssemblyFile(size_t*   instructions_count,
+                 uint8_t** instructions)
+{
+    ASSERT(instructions_count);
+
+    uint64_t compiler_version = 0;
 
     FILE* assembled_file = fopen(ASSEMBLED_FILE_NAME, "rb");
     if (assembled_file == NULL)
     {
-        printf(RED "ASSEMBLED FILE WRITE ERROR.\n" STANDARD);
-
-        return DISASSEMBLER_MAIN_RETURN_ASSEMBLED_FILE_OPEN_ERROR;
+        return DISASSEMBLER_RETURN_ASSEMBLED_FILE_OPEN_ERROR;
     }
 
     fread(&compiler_version , sizeof(uint64_t), 1, assembled_file);
     if (compiler_version != DISASSEMBLER_VERSION)
     {
         printf(RED "VERSION OF COMPILER IS MISMATCHING WITH VERSION OF DISASSEMBLER.\n" STANDARD);
-        return DISASSEMBLER_MAIN_RETURN_VERSION_MISMATCH;
+        return DISASSEMBLER_RETURN_VERSION_MISMATCH;
     }
 
-    fread(&instructions_count , sizeof(size_t), 1, assembled_file);
+    fread(instructions_count , sizeof(size_t), 1, assembled_file);
 
-    uint8_t* instructions = (uint8_t*) calloc(instructions_count, sizeof(uint8_t));
-    fread(instructions, sizeof(uint8_t), instructions_count, assembled_file);
+    *instructions = (uint8_t*) calloc(*instructions_count, sizeof(uint8_t));
+    fread(*instructions, sizeof(uint8_t), *instructions_count, assembled_file);
 
-    if (fclose(assembled_file) != (int) DISASSEMBLER_MAIN_RETURN_SUCCESS)
+    if (fclose(assembled_file) != 0)
     {
-        printf(RED "ASSEMBLED FILE WRITE ERROR.\n" STANDARD);
-
-        return DISASSEMBLER_MAIN_RETURN_ASSEMBLED_FILE_OPEN_ERROR;
+        return DISASSEMBLER_RETURN_ASSEMBLED_FILE_OPEN_ERROR;
     }
+
+    return DISASSEMBLER_RETURN_SUCCESS;
+}
+
+static disassembler_return_e
+WriteDisassembledFile(size_t   instructions_count,
+                      uint8_t* instructions)
+{
+    ASSERT(instructions);
 
     FILE* disassembled_file = fopen(DISASSEMBLED_FILE_NAME, "w+");
     if (disassembled_file == NULL)
     {
-        free(instructions);
-        printf(RED "DISASSEMBLED FILE WRITE ERROR.\n" STANDARD);
-
-        return DISASSEMBLER_MAIN_RETURN_DISASSEMBLED_FILE_OPEN_ERROR;
+        return DISASSEMBLER_RETURN_DISASSEMBLED_FILE_OPEN_ERROR;
     }
 
     for(size_t command_index = 0; command_index < instructions_count;)
@@ -78,15 +111,10 @@ main(int                argc,
         (DISASSEMBLER_COMMANDS_ARRAY[TranslateCommandNumber(instructions, &command_index)].binary_handler)(instructions, &command_index, disassembled_file);
     }
 
-    if (fclose(disassembled_file) != (int) DISASSEMBLER_MAIN_RETURN_SUCCESS)
+    if (fclose(disassembled_file) != (int) DISASSEMBLER_RETURN_SUCCESS)
     {
-        free(instructions);
-        printf(RED "DISASSEMBLED FILE WRITE ERROR.\n" STANDARD);
-
-        return DISASSEMBLER_MAIN_RETURN_DISASSEMBLED_FILE_OPEN_ERROR;
+        return DISASSEMBLER_RETURN_DISASSEMBLED_FILE_OPEN_ERROR;
     }
 
-    free(instructions);
-
-    return DISASSEMBLER_MAIN_RETURN_SUCCESS;
+    return DISASSEMBLER_RETURN_SUCCESS;
 }
--- src/processor/processor.cpp
+++ src/processor/processor.cpp
@@ -10,43 +10,35 @@
  *Now, only god knows it!
  */
 
-enum
-processor_main_return_e
-{
-    PROCESSOR_MAIN_RETURN_SUCCESS,
-    PROCESSOR_MAIN_RETURN_FILE_READ_ERROR,
-    PROCESSOR_MAIN_RETURN_INIT_ERROR,
-    PROCESSOR_MAIN_RETURN_EXECUTION_ERROR
-};
-
 const char* ASSEMBLED_FILE_NAME = "compiled.obj";
 
 int
 main(int                argc,
      const char* const* argv)
 {
+    int output = 0;
     spu_t processor = {};
 
-    if (ReadFlags(argc, argv, &ASSEMBLED_FILE_NAME, &ASSEMBLED_FILE_NAME) != (int) PROCESSOR_MAIN_RETURN_SUCCESS)
+    if ((output = ReadFlags(argc, argv, &ASSEMBLED_FILE_NAME, &ASSEMBLED_FILE_NAME)) != 0)
     {
-        return PROCESSOR_MAIN_RETURN_FILE_READ_ERROR;
+        return output;
     }
 
-    if (InitializeSPU(&processor, ASSEMBLED_FILE_NAME) != (int) PROCESSOR_MAIN_RETURN_SUCCESS)
+    if ((output = InitializeSPU(&processor, ASSEMBLED_FILE_NAME)) != 0)
     {
         DestroySPU(&processor);
-        return PROCESSOR_MAIN_RETURN_INIT_ERROR;
+        return output;
     };
 
-    if (ExecuteInstructions(&processor) != (int) PROCESSOR_MAIN_RETURN_SUCCESS)
+    if ((output = ExecuteInstructions(&processor)) != 0)
     {
         DestroySPU(&processor);
-        return PROCESSOR_MAIN_RETURN_EXECUTION_ERROR;
+        return output;
     };
 
     DestroySPU(&processor);
 
-    return PROCESSOR_MAIN_RETURN_SUCCESS;
+    return 0;
 }
 
 
